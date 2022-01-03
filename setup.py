cb69f40cd122703ce3058df36416c8119b38443f
pattonw <pattonw@hhmi.org>
2022-01-03 11:32:04 -0500

    change package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="secret-santa",
+    name="ss",
     version="0.1",
     url="https://github.com/pattonw/secret-santa",
     author="William Patton",
